e7de1d4e49d527515c4584ebf251929417e1ab81
saribalazs <71698313+saribalazs@users.noreply.github.com>
2020-11-03 13:31:14 +0100

    Add files via upload

---
+++ f0004b.py
@@ -0,0 +1,9 @@
+név = input('Hogy híjják kendet? ')
+kor = input('És hány éves kend? ') 
+kor = int(kor) 
+év = input('Melyik év van? ')
+év = int(év)
+születési_év = év - kor
+print(név, ', kend ', születési_év, '-ban született.', sep='')
+érettségi_év = születési_év + 18
+print(név, ', kend ', érettségi_év, '-ban érettségizik.', sep='') 
